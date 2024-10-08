9225006c18d86348723cbf87ab4a794f291e7529
João Nogueira <22820003+c4f3z1n@users.noreply.github.com>
2024-10-08 16:39:12 +0200

    nix/flake.lock: Update
    Flake lock file updates:
    
    • Removed input 'flake-parts'
    • Removed input 'flake-parts/nixpkgs-lib'
    • Removed input 'systems'

--- nix/flake.nix
+++ nix/flake.nix
@@ -3,23 +3,9 @@
 
   inputs = {
     # deduplication;
-    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
     sops-nix.inputs.nixpkgs-stable.follows = "nixpkgs";
     sops-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs @ {
-    flake-parts,
-    nixpkgs,
-    sops-nix,
-    systems,
-    ...
-  }:
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      flake = {lib, ...}: {
-        nixosConfigurations = import ./hosts.nix {inherit lib sops-nix;};
-      };
-
-      systems = import systems;
-    };
+  outputs = {nixpkgs, ...}: {};
 }
